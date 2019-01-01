9de17e22a1fe03859b89fbd3c806a94a2080ebbf
刘思晗-mac15 <evaseemefly@126.com>
2019-01-01 19:11:29 +0800

    1-加入通过vue+ts重写的项目

---
+++ webclientbyts/webclientbyts/src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+<script lang="ts" src="./about.ts">
+
+// import { Component, Vue } from 'vue-property-decorator';
+// import About from '@/components/About.vue'; // @ is an alias to /src
+
+// @Component({
+//   components: {
+//     About,
+//   },
+// })
+// export default class Home extends Vue {}
+</script>
